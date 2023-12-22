645c724f59d21acc1f7087cc1dc89f563247486f
kaiyuskis <yuki_skier@icloud.com>
2023-12-22 14:42:54 +0900

    bugfix

--- app.py
+++ app.py
@@ -254,5 +254,34 @@ def video(id):
 
         return render_template('video.html', video=video)
 
+#検証要
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
+
+@app.route('/index3')
+def index3():
+    return render_template('index3.html')
+
+@app.route('/index4')
+def index4():
+    return render_template('index4.html')
+
+@app.route('/index5')
+def index5():
+    return render_template('index5.html')
+
+@app.route('/index6')
+def index6():
+    return render_template('index6.html')
+
+@app.route('/index7')
+def index7():
+    return render_template('index7.html')
+
+@app.route('/index8')
+def index8():
+    return render_template('index8.html')
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
